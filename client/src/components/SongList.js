c995268dc65dc3eedf90435212133c9c518a0425
dhakalsamundra <seapush02@gmail.com>
2020-12-21 14:38:05 +0200

    song list component

---
+++ client/src/components/SongList.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+import { getAll } from '../reducers/songReducer'
+export default function SongList() {
+    const dispatch = useDispatch()
+    const items = useSelector((state) => state.data)
+    useEffect(() => {
+        dispatch(getAll())
+      }, [dispatch])
+    return (
+        <div>
+            {items.map((song) => (
+            <h1>title: {song.title}</h1>
+            ))}
+        </div>
+    )
+}
