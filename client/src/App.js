37720fa395303df2ef3dca4929324e981955a0e4
dhakalsamundra <seapush02@gmail.com>
2020-12-21 14:39:45 +0200

    refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import SongList from './components/SongList'
 
 function App() {
   return (
     <div className="App">
       <h1>New Songs Delivered Every Week</h1>
+      <SongList />
     </div>
   );
 }
