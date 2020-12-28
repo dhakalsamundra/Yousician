57bd47c77d9993c4d8debaea9e2a5920be2fc66f
dhakalsamundra <seapush02@gmail.com>
2020-12-28 09:20:08 +0200

    add serch component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Songs from '../components/Songs'
-import SongFilter from '../components/Search'
+import Search from '../components/Search'
 
 const Home = () => {
   return (
     <div className='grid-2'>
       <div>
-        <SongFilter />
+        <Search />
         <Songs />
       </div>
     </div>
