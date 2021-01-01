9ec431cb5d9166e9ef8f007340ac7fdac332387a
dhakalsamundra <seapush02@gmail.com>
2021-01-01 15:47:01 +0200

    refactor

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,29 @@
 import React from 'react'
+
 import Songs from '../components/Songs'
 import Search from '../components/Search'
+import Filter from '../assets/icons/filter.svg'
 
 const Home = () => {
   return (
     <div className='grid-2'>
       <div>
         <Search />
+        <div className='filter'>
+
+          <span>FILTER BY LEVEL</span>
+
+          <div className='filter-icon'>
+
+            <p>5 - 10</p>
+
+            <div className='img-filter'>
+
+            <img src={Filter} alt='filter icon' />
+            
+            </div>
+          </div>
+        </div>
         <Songs />
       </div>
     </div>
